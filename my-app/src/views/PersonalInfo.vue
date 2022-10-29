aafaa7012d97d79b7b90db3c99a2ea4ff32c178d
ahao268 <2245157316@qq.com>
2022-10-29 18:36:24 +0800

    v-model在父子组件中的使用的demo

---
+++ my-app/src/views/PersonalInfo.vue
@@ -0,0 +1,35 @@
+<!-- 双向绑定demo -->
+<template>
+  <div>
+    <input
+      type="number"
+      :value="phoneInfo.phone"
+      @change="handlePhoneChange"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  model: {
+    prop: "phoneInfo",
+    event: "change",
+  },
+  props: {
+    phoneInfo: Object,
+  },
+  methods: {
+    handlePhoneChange(e) {
+      if (e.target.value === "") {
+        console.log("不能为空")
+      }
+      this.$emit("change", {
+        ...this.phoneInfo,
+        phone: e.target.value,
+      })
+    },
+  },
+}
+</script>
+
+<style></style>
--- my-app/src/views/index.vue
+++ my-app/src/views/index.vue
@@ -2,23 +2,35 @@
 <template>
   <div>
     <!-- <Props :type="type" :is-visible="false" :onChange="handleProChange"></Props> -->
-    <Event :name="name" @changeName="changeName"></Event>
+    <!-- <Event :name="name" @changeName="changeName"></Event> -->
+    <PersonalInfo v-model="phoneInfo"></PersonalInfo>
+    <PersonalInfo
+      :phone-info="phoneInfo"
+      @change="(val) => (phoneInfo = val)"
+    ></PersonalInfo>
+    phoneInfo: {{ phoneInfo }}
   </div>
 </template>
 
 <script>
 // import Props from "./Props.vue"
-import Event from "./Event.vue"
+// import Event from "./Event.vue"
+import PersonalInfo from "./PersonalInfo.vue"
 export default {
   data() {
     return {
       type: "success",
       name: "hello",
+      phoneInfo: {
+        areaCode: "+86",
+        phone: "",
+      },
     }
   },
   components: {
     // Props,
-    Event,
+    // Event,
+    PersonalInfo,
   },
   methods: {
     handleProChange() {},
